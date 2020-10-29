ee1034592a7763a685ffaa9e09e9805af7afa256
Marzenah <marzenah123@gmail.com>
2020-10-29 01:13:00 -0400

    pp

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,13 +1,22 @@
 function convertRestaurantsToCategories(restaurantList) {
   // process your restaurants here!
-  return list;
+  const catList = [];
+  
+  const groupedByCat = restaurantList.reduce((categList, item) => {
+    if(!(item.category in categList)) {
+      const newCat = item.category;
+      catList.push(newCat);
+      groupedByCat.push({category: newCat, cunt: 1}); 
+    } else {
+      groupedByCat.count +=1;
+    }
+  }
+  return groupedByCat;
 }
 
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
-  CanvasJS.addColorSet('customColorSet1', [
-    // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
-  ]);
+  CanvasJS.addColorSet('customColorSet1', ['#F4FA0D', '#186ED6', '#E326ED', '#00DE19', '#EB201C', '#702319', '#255BFS', '#20F03C', '#EB23AE', '#4C6275', '#F00000']);
 
   return {
     animationEnabled: true,
@@ -24,7 +33,24 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
       gridColor: 'rgba(1,77,101,.1)',
       title: 'Change This Title',
       labelFontSize: 12,
-      scaleBreaks: {customBreaks: []} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+      scaleBreaks: {type: 'wavy', customBreaks: [{
+				startValue: 0,
+				endValue: 50
+				},
+				{
+					startValue: 51,
+					endValue: 100
+        },
+        {
+					startValue: 101,
+					endValue: 150
+        },
+        {
+					startValue: 151,
+					endValue: 200
+        }
+
+      ]} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
     },
     data: [{
       type: 'bar',
@@ -40,48 +66,6 @@ function runThisWithResultsFromServer(jsonFromServer) {
   sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
 
   // Process your restaurants list
-  const chart = new CanvasJS.Chart("chartContainer", {
-    animationEnabled: true,
-    
-    title:{
-      text:"Fortune 500 Companies by Country"
-    },
-    axisX:{
-      interval: 1
-    },
-    axisY2:{
-      interlacedColor: "rgba(1,77,101,.2)",
-      gridColor: "rgba(1,77,101,.1)",
-      title: "Number of Companies"
-    },
-    data: [{
-      type: "bar",
-      name: "companies",
-      axisYType: "secondary",
-      color: "#014D65",
-      dataPoints: [
-        { y: 3, label: "Sweden" },
-        { y: 7, label: "Taiwan" },
-        { y: 5, label: "Russia" },
-        { y: 9, label: "Spain" },
-        { y: 7, label: "Brazil" },
-        { y: 7, label: "India" },
-        { y: 9, label: "Italy" },
-        { y: 8, label: "Australia" },
-        { y: 11, label: "Canada" },
-        { y: 15, label: "South Korea" },
-        { y: 12, label: "Netherlands" },
-        { y: 15, label: "Switzerland" },
-        { y: 25, label: "Britain" },
-        { y: 28, label: "Germany" },
-        { y: 29, label: "France" },
-        { y: 52, label: "Japan" },
-        { y: 103, label: "China" },
-        { y: 134, label: "US" }
-      ]
-    }]
-  });
-  chart.render();
   // Make a configuration object for your chart
   // Instantiate your chart
   const reorganizedData = convertRestaurantsToCategories(jsonFromServer);
@@ -90,7 +74,7 @@ function runThisWithResultsFromServer(jsonFromServer) {
   chart.render();
 }
 
-// Leave lines 52-67 alone; do your work in the functions above
+/// Leave lines 52-67 alone; do your work in the functions above
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray();
