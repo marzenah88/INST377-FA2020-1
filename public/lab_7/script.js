81340b5898e494aac2f099d7396a04c343d48077
Marzenah <marzenah123@gmail.com>
2020-10-29 15:03:08 -0400

    oo

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -2,7 +2,7 @@ function convertRestaurantsToCategories(restaurantList) {
   const categories = restaurantList.reduce((cat_list, restaurant) => {
     const checkCat = cat_list.find((category) => category.label === item.category);
     if (!checkCat) {
-      cat_list.push({label: item.category, y: 1});
+      cat_list.push({label: restaurant.category, y: 1});
     } else {
       checkCat.y +=1;
     }
