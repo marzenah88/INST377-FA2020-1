d57e2ea47ccbe423aeec2e7725cbd2be59d362ad
Marzenah <marzenah123@gmail.com>
2020-11-07 23:03:57 -0500

    mmm

--- public/asst-2/script.js
+++ public/asst-2/script.js
@@ -1,49 +1,41 @@
-function shringItemData(restaurantList) {
-  const categories = restaurantList.map(x => [x.name, x.address_line1, x.address_line2, x.city, x.state, x.zip]);
-  
-  return categories;
-}
-function runDataFromServer(jsonFromServer) {
-  console.log('jsonFromServer', jsonFromServer);
-  sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer));
+onst lab_data = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-  const matchResults = shringItemData(jsonFromServer);
-  console.log(matchResults);
-}
+const venues = [];
 
-document.body.addEventListener('submit', async (e) => {
-  e.preventDefault(); 
-  const form = (e.target).serializeArray();
-  const venues = []; 
-  fetch('/api', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(form)
-  })
-    .then((fromServer) => fromServer.json())
-    .then((jsonFromServer) => venues.push(...jsonFromServer))
-    .then((venues) => matching(venues))
-    .catch((err) => {
-      console.log(err);
-    });
-});
-function matching(venues) {
-  const searchInput = document.querySelector('.searchbar');
-  const suggestions = document.querySelector('.suggestions');
-  
-  const results = findMatches(searchInput, venues);
+fetch(labs_data)
+    .then(blob => blob.json())
+    .then(data => cities.push(...data))
+    ;
 
+function findMatches(wordsToMatch, venues) {
+    return venues.filter(venue => {
+        const regex = new RegExp(wordsToMatch, 'gi');
+        if (wordsToMatch === "") {
+            return null
+        } else {
+            return venue.name.match(regex) || venue.city.match(regex);
+        }
+    })
 }
 
-function findMatches(word, venues) {
-  return venues.filter (venue => {
-    const regex = new RegExp(word, 'gi');
-    return venues.name.match(regex) || venues.city.match(regex)
-  });
-}
 function displayMatches() {
-  const matchArray = findMatches(this.value, venues);
-  console.log(matchArray);
+    const matchArray = findMatches(this.value, venues);
+    const html = matchArray.map(venue => {
+        const regex = new RegExp(this.value, 'gi');
+        const venueName = venue.name.replace(regex, `<span class="highlightme">${this.value}</span>`);
+        const venueCity = venue.city.replace(regex, `<span class="highlightme">${this.value}</span>`);
+        return `
+            <li>
+                <span class="population">${venueName}</span>
+                <span class="name">${venueCity}, ${venue.zip}</span>
+                <span class="population">${venue.category}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
 }
+
+/*wesbos' input had a class of 'search' but ours is textinput */
+const searchInput = document.querySelector('.textinput');
+const suggestions = document.querySelector('.suggestions');
+searchInput.addEventListener('input', displayMatches);
