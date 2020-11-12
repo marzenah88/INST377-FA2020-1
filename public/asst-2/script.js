ceca6883e77281376af04f05a94d141d2f5abd22
Marzenah <marzenah123@gmail.com>
2020-11-11 22:04:23 -0500

    oo

--- public/asst-2/script.js
+++ public/asst-2/script.js
@@ -4,35 +4,34 @@ const venues = [];
 
 fetch(lab_data)
   .then(blob => blob.json())
-  .then(data => venues.push(...data))
-  ;
+  .then(data => venues.push(...data));
 
 function findMatches(wordsToMatch, venues) {
   return venues.filter(venue => {
     const regex = new RegExp(wordsToMatch, 'gi');
     if (wordsToMatch === "") {
-      return null
-    } else {
-      return venue.name.match(regex) || venue.city.match(regex);
+      return null;
     }
+    return venue.name.match(regex) || venue.city.match(regex);
   });
 }
 
 function displayMatches() {
   const matchArray = findMatches(this.value, venues);
   const html = matchArray.map(venue => {
-      const regex = new RegExp(this.value, 'gi');
-      const venueName = venue.name.replace(regex, `<span class="highlightme">${this.value}</span>`);
-      const venueCity = venue.city.replace(regex, `<span class="highlightme">${this.value}</span>`);
-      return `
-            <li>
-                <span class="population">${venueName}</span>
-                <span class="name">${venueCity}, ${venue.zip}</span>
-                <span class="population">${venue.category}</span>
-            </li>
-        `;
-    }).join('');
-    suggestions.innerHTML = html;
+    const regex = new RegExp(this.value, 'gi');
+    const venueName = venue.name.replace(regex, `<span class="highlightme">${this.value}</span>`);
+    const venueCity = venue.city.replace(regex, `<span class="highlightme">${this.value}</span>`);
+    return `
+      <li>
+        <span class="population">${venueName}</span>
+        <span class="name">${venueCity}, ${venue.zip}</span>
+        <span class="population">${venue.category}</span>
+        <span class="population">${venue.inspection_results}</span>
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
 }
 
 /*wesbos' input had a class of 'search' but ours is textinput */
