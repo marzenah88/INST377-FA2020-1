5941824f086626195b402b801a27079aa2bf8e4d
Marzenah <marzenah123@gmail.com>
2020-11-12 00:14:33 -0500

    nn

--- public/asst-2/script.js
+++ public/asst-2/script.js
@@ -1,27 +1,23 @@
-const lab_data = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const pg_data = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
 const venues = [];
 
-fetch(lab_data)
-  .then(lab_data => lab_data.json())
-  .then(dataset => venues.push(...dataset));
+fetch(pg_data)
+  .then(blob => blob.json())
+  .then(data => venues.push(...data))
 
 function findMatches(wordsToMatch, venues) {
   return venues.filter(venue => {
     const regex = new RegExp(wordsToMatch, 'gi');
-    if (wordsToMatch === "") {
-      return null;
-    }
-    return venue.name.match(regex) || venue.city.match(regex);
+    return venue.name.match(regex) || venue.city.match(regex)
   });
 }
-
 function displayMatches() {
   const matchArray = findMatches(this.value, venues);
   const html = matchArray.map(venue => {
     const regex = new RegExp(this.value, 'gi');
-    const venueName = venue.name.replace(regex, `<span class="highlightme">${this.value.ignoreCase}</span>`);
-    const venueCity = venue.city.replace(regex, `<span class="highlightme">${this.value.ignoreCase}</span>`);
+    const venueName = venue.name.replace(regex, `<span class="highlightme">${this.value}</span>`);
+    const venueCity = venue.city.replace(regex, `<span class="highlightme">${this.value}</span>`);
     return `
       <li>
         <span class="name">${venueName},  </span>
@@ -35,4 +31,4 @@ function displayMatches() {
 }
 const searchInput = document.querySelector('.textinput');
 const suggestions = document.querySelector('.suggestions');
-searchInput.addEventListener('input', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
